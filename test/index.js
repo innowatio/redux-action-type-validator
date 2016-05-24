ce0b9bb63338be1e1319f00599be52cd6da3b71a
Davide Bianchi <davide.bianchi@mondora.com>
2016-05-24 17:37:12 +0200

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,31 @@
+import {expect} from "chai";
+import {String, Number, tuple} from "tcomb";
+
+import actionTypeValidator from "index";
+
+describe("`actionTypeValidator` function", () => {
+
+    it("should return a function", () => {
+        const ret = actionTypeValidator();
+        expect(ret).to.be.an.instanceOf(Function);
+    });
+
+    it("should throw an `Error` if the types of the args aren't the expected types", () => {
+        const retFunction = actionTypeValidator(tuple([String, Number]));
+        const argument = ["firstArgument", "seconddArgument"];
+        function troublemaker () {
+            retFunction(argument);
+        }
+        expect(troublemaker).to.throw();
+    });
+
+    it("shouldn't throw an `Error` if the types of the args are the expected types", () => {
+        const retFunction = actionTypeValidator(tuple([String, Number]));
+        const argument = ["firstArgument", 123];
+        function troublemaker () {
+            retFunction(argument);
+        }
+        expect(troublemaker).not.to.throw();
+    });
+
+});
